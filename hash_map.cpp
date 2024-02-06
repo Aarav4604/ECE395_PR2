b5440408ec23920e1947a33cf97279ab1fbc731a
ZachBeacon <89481556+ZachBeacon@users.noreply.github.com>
2024-02-06 17:29:17 -0500

    Not Complete, 3 functions done
    I completed the hash map copy function, insert, and get value functions.

--- hash_map.cpp
+++ hash_map.cpp
@@ -10,21 +10,51 @@ hash_map::hash_map(size_t capacity){
 }
 
 
-hash_map::hash_map(const hash_map &other){
+hash_map::hash_map(const hash_map &other)
+{
+    _size = other._size;
+    _capacity = other._capacity;
+    _head = new hash_list[_capacity];
+
+    for(int i = 0; i <= _size; i++)
+    {
+        this->_head[i] = other._head[i];        
+    }
 
 }
 
-hash_map& hash_map:: operator=(const hash_map &other){
+hash_map& hash_map:: operator=(const hash_map &other)
+{
 
 }
 
-void hash_map::insert(int key, float value) {
+void hash_map::insert(int key, float value) 
+{
+    int index_hash = abs(key) % _capacity;
+    int temp_size = _head[index_hash].get_size();
+    _head[index_hash].insert(key, value);
+
+    if(temp_size != _head[index_hash].get_size())
+    {
+        _size = _size + 1;
+    }   
 }
 
-std::optional<float> hash_map::get_value(int key) const {
+std::optional<float> hash_map::get_value(int key) const 
+{
+    for(int i = 0; i<= _size; i++)
+    {
+        
+        if(_head[i].get_value(key).has_value())
+        {
+            return _head[i].get_value(key);
+        }
+    }
+    return std::nullopt;
 }
 
-bool hash_map::remove(int key){
+bool hash_map::remove(int key)
+{
 
 }
 
@@ -37,7 +67,10 @@ size_t hash_map::get_capacity() const {
     return _capacity;
 }
 
-void hash_map::get_all_keys(int *keys) {
+void hash_map::get_all_keys(int *keys) 
+{
+    
+
 }
 
 void hash_map::get_bucket_sizes(size_t *buckets) {
