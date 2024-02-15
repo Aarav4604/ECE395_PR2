00e231dea46a50996de18ac6b5e479ed31163dc0
Aarav Patel <aaravpatel@Aaravs-MacBook-Pro-2.local>
2024-02-15 18:31:08 -0500

    final

--- hash_p2/hash_map2.hpp
+++ hash_p2/hash_map2.hpp
@@ -8,6 +8,7 @@ Still a lot left to do, I tried to get some of the base code in it. The files in
 
 */
 
+template<typename K, typename V>
 hash_map::hash_map(size_t capacity, float upper_load_factor, float lower_load_factor){
     _size = 0; 
     _capacity = capacity; 
@@ -18,19 +19,42 @@ hash_map::hash_map(size_t capacity, float upper_load_factor, float lower_load_fa
 }
 
 
-hash_map<K,V>::hashmap(const hash_map &other)
-{
-
+template<typename K, typename V>
+hash_map<K, V>::hash_map(const hash_map &other)
+    : _upper_load_factor(other._upper_load_factor),
+      _lower_load_factor(other._lower_load_factor),
+      _size(other._size),
+      _capacity(other._capacity),
+      capacity_index(other.capacity_index) {
+    _head = new hash_list<K, V>[_capacity];
+    for (size_t i = 0; i < _capacity; ++i) {
+        _head[i] = other._head[i]; 
+    }
 }
 
-template<typename K, typename V> hash_map &hash_map<K,V>::operator=(const hash_map &other)
-{
-
+template<typename K, typename V>
+hash_map<K, V>& hash_map<K, V>::operator=(const hash_map &other) {
+    if (this != &other) {
+        delete[] _head;
+        _capacity = other._capacity;
+        _size = other._size;
+        _upper_load_factor = other._upper_load_factor;
+        _lower_load_factor = other._lower_load_factor;
+        capacity_index = other.capacity_index;
+        _head = new hash_list<K, V>[_capacity];
+        for (size_t i = 0; i < _capacity; ++i) {
+            _head[i] = other._head[i]; 
+        }
+    }
+    return *this;
 }
 
-template<typename K, typename V> void hash_map<K,V>::insert(K key, V value)
-{
-
+template<typename K, typename V>
+void hash_map<K, V>::insert(K key, V value) {
+    size_t index = _hash(key) % _capacity;
+    _head[index].insert(key, value); 
+    _size++;
+    
 }
 
 template<typename K, typename V> std::optional<V> hash_map<K,V>::get_value(K key) const
@@ -43,9 +67,14 @@ template<typename K, typename V> std::optional<V> hash_map<K,V>::get_value(K key
     return std::nullopt;
 }
 
-template<typename K, typename V> bool hash_map<K,V>::remove(K key)
-{
-
+template<typename K, typename V>
+bool hash_map<K, V>::remove(K key) {
+    size_t index = _hash(key) % _capacity;
+    if (_head[index].remove(key)) {
+        _size--;
+        return true;
+    }
+    return false;
 }
 
 template<typename K, typename V> size_t hash_map<K,V>::get_size() const 
@@ -73,9 +102,10 @@ template<typename K, typename V> void hash_map<K,V>::get_all_keys(K *keys)
     }
 }
 
-template<typename K, typename V> void hash_map<K,V>::get_all_sorted_keys(K *keys)
-{
-
+template<typename K, typename V>
+void hash_map<K, V>::get_all_sorted_keys(K *keys) {
+    get_all_keys(keys);
+    std::sort(keys, keys + _size); 
 }
 
 template<typename K, typename V> void hash_map<K,V>::get_bucket_sizes(size_t *buckets)
